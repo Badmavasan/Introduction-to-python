da72ebb04532dc599486c40b9f1ae6cbcb519679
Badmavasan <93829815+Badmavasan@users.noreply.github.com>
2024-09-27 15:26:18 +0200

    Adding tp note 2

---
+++ tp_note2.py
@@ -0,0 +1,14 @@
+# Énoncé:
+# 1. Récupérer les données d'un fichier nommé 'data.txt'. Ce fichier contient un entier par ligne.
+# 2. Stocker les valeurs lues dans une liste d'entiers.
+# 3. Trier cette liste sans utiliser les fonctions 'sorted()' ou 'sort()'. 
+# 4. Utiliser une double boucle 'for' pour implémenter l'algorithme de tri par sélection.
+# 5. Enregistrer la liste triée dans un autre fichier nommé 'sorted_data.txt', un entier par ligne.
+
+# Pseudo-code pour le tri par sélection :
+# 1. Pour chaque élément i de la liste :
+#    a. Trouver le plus petit élément à partir de l'indice i jusqu'à la fin de la liste.
+#    b. Échanger l'élément à l'indice i avec cet élément minimum.
+# 2. Répéter jusqu'à ce que la liste soit complètement triée.
+
+# Si jamais vous avez du mal avec la gestion du fichier vous pouvez créer une liste d'entier et fair ele tri de cette liste sans utiliser sort ou sorted.
